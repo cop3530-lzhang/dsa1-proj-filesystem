3782523cddd4bb19e99884fccf2bf7351a15d23e
Ian Fang <xfang@uwf.edu>
2026-08-26 20:11:40 -0500

    fix(gitignore): un-ignore test C++ source files and track test suite

---
+++ test/test-1-node.cpp
@@ -0,0 +1,31 @@
+#include "../node.hpp"
+#include "catch/catch.hpp"
+
+TEST_CASE("Node") {
+  Node n("root", 'd');
+  CHECK("root" == n.getName());
+  CHECK('d' == n.getType());
+  CHECK(nullptr == n.getParent());
+  CHECK(0 == n.getChildren().size());
+
+  Node* child = new Node("child", 'f');
+  n.addChild(child);
+  CHECK(child == n.getChild("child"));
+  CHECK((&n == child->getParent()));
+
+  n.addChild("second", 'd');
+  CHECK('d' == n.getChild("second")->getType());
+
+  Node* parent = new Node("parent", 'd');
+  n.setParent(parent);
+  CHECK(parent == n.getParent());
+
+  CHECK(2 == n.getChildren().size());
+
+  /* Verify removal of child and all successors */
+  n.getChild("second")->addChild("seconds-new-baby", 'f');  // add grandchild
+  CHECK(n.removeChild("second"));  // should remove second and seconds-new-baby
+  CHECK(1 == n.getChildren().size());
+
+  delete parent;
+}
